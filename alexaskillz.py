3b229a6560864b3597a3897d70f2657aad0acb14
William Chislett <wchislett@sorensonmedia.com>
2017-03-03 17:35:02 +0000

    using wiki api to retrive neoconservatism example

--- alexaskillz.py
+++ alexaskillz.py
@@ -1,4 +1,5 @@
 import os
+import urllib, json
 from flask import Flask
 from flask_ask import Ask, statement
 
@@ -6,9 +7,21 @@ app = Flask(__name__)
 ask = Ask(app, '/')
 
 @ask.intent('GiveRandomInfo')
-def hello():
-    speech_text = "Neoconservatism is a branch of conservatism"
+def politics_fact():
+
+	politics_title = 'neoconservatism'
+
+	wiki_api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='
+    
+    json_reponse = read_from_url(wiki_api + politics_title)
+
+    speech_text = json_reponse['extract']
+    
     return statement(speech_text)
+
+def read_from_url(url)
+	response = urllib.urlopen(url)
+	return json.loads(response.read())
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
