c6d0cfc19ad07beb820047775418de4a952c5481
William Chislett <wchislett@sorensonmedia.com>
2017-03-03 13:04:20 +0000

    testing with hello world alexa greeting

--- alexaskillz.py
+++ alexaskillz.py
@@ -5,10 +5,10 @@ from flask_ask import Ask, statement
 app = Flask(__name__)
 ask = Ask(app, '/')
 
-@ask.intent('HelloIntent')
-def hello(firstname):
-    speech_text = "Hello %s" % firstname
-    return statement(speech_text).simple_card('Hello', speech_text)
+@ask.intent('GiveRandomInfo')
+def hello():
+    speech_text = "Neoconservatism is a branch of conservatism"
+    return statement(speech_text).simple_card(speech_text)
 
 if __name__ == '__main__':
     app.run()
