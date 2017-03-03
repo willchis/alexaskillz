e3663cc9f63b06e1dbcbdf694b0a9265be852585
William Chislett <wchislett@sorensonmedia.com>
2017-03-03 13:18:20 +0000

    testing on different ip

--- alexaskillz.py
+++ alexaskillz.py
@@ -8,7 +8,7 @@ ask = Ask(app, '/')
 @ask.intent('GiveRandomInfo')
 def hello():
     speech_text = "Neoconservatism is a branch of conservatism"
-    return statement(speech_text).simple_card(speech_text)
+    return statement(speech_text)
 
 if __name__ == '__main__':
     app.run()
