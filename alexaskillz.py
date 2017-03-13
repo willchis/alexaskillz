8908d1d7f4e94d174b005e59b28aa828e35bbb2c
William Chislett <willchis@hotmail.com>
2017-03-13 22:06:33 +0000

    Adding MSW api calls for current surf conditions.

--- alexaskillz.py
+++ alexaskillz.py
@@ -1,4 +1,5 @@
 import os
+import time
 import urllib, json
 from random import randint
 
@@ -8,20 +9,47 @@ from flask_ask import Ask, statement, question
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+MAGIC_SEAWEED_KEY = 'e96ca9b1f03a627aaab136eafe4f83ee'
+SPOT_ID = '1449'
 
 @ask.launch
 def launched():
-    return question("Welcome to the Level Project. More features will be coming soon. In the meantime, get political facts by saying: Alexa, ask the level project for a fact... Would you like a demo now?")
+    return question("The is the surf report for Restbay, Porthcawl, South Wales.  Would you like today's surf report?")
 
-@ask.intent('YesOrNoFIrstIntent', mapping={'answer': 'yesorno'})
+@ask.intent('YesOrNoFIrstIntent', mapping = { 'answer': 'yesorno' })
 def yes_or_no(answer):
     statement('Okay')
-    if(answer == 'yes'):  
-        return politics_fact()
+    if (answer == 'yes'):  
+        return surf_report('today')
     else:
         return
 
-@ask.intent('GiveRandomInfo')
+@ask.intent('SwellIntent', mapping = { 'report_time': 'timeofday'})
+def current_surf(report_time):
+
+    api_url = 'http://magicseaweed.com/api/%s/forecast/?spot_id=%s&fields=timestamp,swell.minBreakingHeight,swell.maxBreakingHeight' % (MAGIC_SEAWEED_KEY, SPOT_ID)
+    print 'Calling Magic Seaweed via API URL: ' + api_url
+    json_reponse = read_from_url(api_url)
+
+    unix_now = time.time()
+    time_difference = 0
+    closet_time = json_reponse[0]
+
+    for item in json_reponse:
+        item_time_difference = abs(item['timestamp'] - unix_now)
+
+        if (item_time_difference <= time_difference):
+            time_difference = item_time_difference
+            closet_time = item
+
+    min_swell = closet_time['swell']['minBreakingHeight']
+    max_swell = closet_time['swell']['maxBreakingHeight']
+    
+    print 'Got swell heights: %d to %d ft.' % (min_swell, max_swell)
+    return statement('Currently the swell height is between %d and %d foot at rest bay.' % (min_swell, max_swell))
+
+
+#@ask.intent('GiveRandomInfo')
 def politics_fact():
 
     #TODO add headers to ensure wiki api allows access
@@ -114,7 +142,8 @@ def get_random_politics_title():
     return options[randint(0, len(options) - 1)]
 
 if __name__ == '__main__':
-    politics_fact()
+    #politics_fact()
+    current_surf('today')
 
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
